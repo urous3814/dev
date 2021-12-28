ae448dc08669d0bb4ed2b94007ef00835b62ade8
UROUS3814 <40907210+urous3814@users.noreply.github.com>
2021-12-28 17:35:13 +0900

    Update 주식차트.py

--- python/주식차트.py
+++ python/주식차트.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
-form datetime import datetime
+from datetime import datetime
 
-df = pd.read_scv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-fig = go.Fegure(data = [go.Candlestick(x = df['DATA'], open = df['AAPL.Open'], high = df['AAPL.High'], low = df['AAPL.Low'], close = df['AAPL.Close'])])
-fig.show
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+fig = go.Figure(data = [go.Candlestick(x = df['Date'], open = df['AAPL.Open'], high = df['AAPL.High'], low = df['AAPL.Low'], close = df['AAPL.Close'])])
+fig.show()
