f8b385f3635803ad88f82ae4f63e68f2c3d04392
UROUS3814 <urous3814@gmail.com>
2022-07-20 08:22:28 +0900

    Add files via upload

---
+++ RADStudio/BlueTooth/보급형 BLUE v3.0(beta)/Project1.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+#pragma hdrstop
+#include <System.StartUpCopy.hpp>
+//---------------------------------------------------------------------------
+USEFORM("Unit3.cpp", blue_v2);
+//---------------------------------------------------------------------------
+extern "C" int FMXmain()
+{
+	try
+	{
+		Application->Initialize();
+		Application->CreateForm(__classid(Tblue_v2), &blue_v2);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
